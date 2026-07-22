ec6f8499752df8ce31cc7a6f3f5471d5f1278e42
DriftWare07 <99690970+DriftWare07@users.noreply.github.com>
2026-07-21 21:55:53 -0500

    more nametag work

--- scripts/nameTag.gd
+++ scripts/nameTag.gd
@@ -2,13 +2,26 @@ extends Label3D
 class_name NameTag
 
 @export var id : int
+var sid
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	billboard = BaseMaterial3D.BILLBOARD_ENABLED
 	id = get_parent().name.to_int()
+	
+	sid = Networking.peer.get_steam_id_for_peer_id(id)
+	
 	Steam.persona_state_change.connect(setNameFromPersona)
+	Steam.requestUserInformation(sid, true)
 
-func setNameFromPersona(steam_id: int, flags: int):
+
+
+func setNameFromPersona(steam_id: int, flags: int = 0):
 	print("got name")
 	#if steam_id == id:
-	text = Steam.getFriendPersonaName(id)
+	Steam.requestUserInformation(sid, true)
+	var n = Steam.getFriendPersonaName(id)
+	
+	if n == "": text = "Loading"
+	else: text = n
+	print(Steam.getFriendPersonaName(id))
+	print(steam_id)
